3106b3db94908138d7a6efcebf938820d1f81d3c
AlexJava12 <110709052+AlexJava12@users.noreply.github.com>
2022-08-06 09:50:39 +0530

    spring repository

---
+++ UserRepository.java
@@ -0,0 +1,12 @@
+package com.springrest.springrest.repo;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.springrest.springrest.entity.UserEntity;
+
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+
+	public List<UserEntity> findByEmail(String email);
+}
