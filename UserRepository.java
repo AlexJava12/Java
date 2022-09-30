8be11505de121c987f7a60730314af6d01907d5d
iyervanugopal9999 <91649338+iyervanugopal9999@users.noreply.github.com>
2022-10-01 01:21:46 +0530

    aws signup dws

--- UserRepository.java
+++ UserRepository.java
@@ -6,6 +6,22 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.springrest.springrest.entity.UserEntity;
 
+int num = 29;
+        boolean flag = false;
+        for(int i = 2; i <= num/2; ++i)
+        {
+            if(num % i == 0)
+            {
+                flag = true;
+                break;
+            }
+        }
+
+        if (!flag)
+            System.out.println(num + " is a prime number.");
+        else
+            System.out.println(num + " is not a prime number.");
+
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
 
 	public List<UserEntity> findByEmail(String email);
